86c0975a9f8c1cd0406573deae8c2cd1b017fec7
Alex <anofryev@gmail.com>
2021-04-21 13:50:21 +0300

    postgresql database configured

--- SMART_app/settings.py
+++ SMART_app/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'SMART_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chest_xray',
+        'USER' : 'postgres',
+        'PASSWORD' : 'blbIb1bl',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 
